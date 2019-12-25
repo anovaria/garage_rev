43458fd220a231257aa22be4f41b0d947a8b40c4
alex <alex@alex-Lenovo-G50-45>
2019-12-25 07:49:42 +0100

    vers0

--- src/main/java/com/mycompany/garage/Garage.java
+++ src/main/java/com/mycompany/garage/Garage.java
@@ -22,17 +22,19 @@ public class Garage {
     }
 
     public Garage() {
-        int i, j = 0;
+        int j = 0;
         char ch, scelta;
         do {
-            Inserisci();
+            Inserisci(j);
             System.out.print("continui?(s/n):");
             scelta = sc.next().charAt(0);
             j++;
-        } while (scelta != 'n' && j < MAX);//fine do-while
+        } while (scelta != 'n');//fine do-while
+        
     }
 
-    private void Inserisci() {
+    private void Inserisci(int p) {
+        
         //inserisco veicoli 
         //throw new IllegalArgumentException("errore");
         Scanner sc = new Scanner(System.in);
@@ -58,11 +60,12 @@ public class Garage {
                     default:
                         throw new IllegalArgumentException("Errore!!");
                 }
-                Auto A = new Auto(0, 4, alimentazione, "fiat", 2000, 1200);
+                Auto A = new Auto(p, 4, alimentazione, "fiat", 2000, 1200);
                 elencoVeicoli[A.posto] = A;
                 System.out.println("Inserito " + A.toString());
+                p++;
                 stampaSituazione();
-                //rimuovi(A.posto);
+                rimuovi(A.posto);
 
             } catch (IllegalArgumentException ex) {
                 System.out.println(ex);
@@ -72,16 +75,22 @@ public class Garage {
 
         } while (invalid);
 
-        Moto M = new Moto(1, 4, "aprilia", 2000, 600);
+        Moto M = new Moto(p, 4, "aprilia", 2000, 600);
         elencoVeicoli[M.posto] = M;
         System.out.println("Inserito " + M.toString());
+        p++;
         stampaSituazione();
-        Furgone F = new Furgone(2, 300, "Ducato", 1995, 2500);
+        Furgone F = new Furgone(p, 300, "Ducato", 1995, 2500);
         elencoVeicoli[F.posto] = F;
         System.out.println("Inserito " + F.toString());
         stampaSituazione();
-        //rimuovi(F.posto);
-        //rimuovi(M.posto);
+        rimuovi(F.posto);
+        rimuovi(M.posto);
+        /*System.out.println(String.valueOf(elencoVeicoli.length));
+        Moto M1 = new Moto(0, 4, "aprilia", 2000, 600);
+        elencoVeicoli[M1.posto] = M1;
+        System.out.println("Inserito " + M1.toString());
+        stampaSituazione();*/
 
     }
 
